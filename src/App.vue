b2897f511447e9b4581af270e13b9e67336cc378
Your Name <you@example.com>
2022-01-09 15:47:33 +0900

    aaa

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <TradingVue />
+  </div>
+</template>
+
+<script>
+import TradingVue from "./components/TradingVue.vue";
+
+export default {
+  name: "App",
+  components: {
+    TradingVue,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
